d195bb5fe3bcca77a1721978f8e80c607348e534
Monnot Stéphane <monnot.stephane@gmail.com>
2016-03-05 17:35:54 +0100

    Add phpdoc block

--- tests/helpers.php
+++ tests/helpers.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Create a user
+ *
+ * @param array $overrides
+ * @param int   $amount
+ *
+ * @return \App\User
+ */
 function createUser($overrides = [], $amount = 1){
     return factory(\App\User::class, $amount)->create($overrides);
 }
