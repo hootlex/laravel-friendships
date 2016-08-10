73bb02547fdcb2d88300e2a78fdc8ff37fabc2f5
= <nikolaynesov@gmail.com>
2016-08-10 15:37:39 +0300

    Update test friendships config

--- tests/config/friendships.php
+++ tests/config/friendships.php
@@ -8,8 +8,9 @@ return [
     ],
 
     'groups' => [
-        'close_friends' => 0,
-        'acquaintances' => 1
+        'acquaintances' => 0,
+        'close_friends' => 1,
+        'family' => 2
     ]
 
 ];
