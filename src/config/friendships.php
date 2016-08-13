7c7790b560a1cc82172a51981ae9a27da3f4a8ec
alex <hootlex@icloud.com>
2016-08-13 17:50:24 +0300

    make all groups active

--- src/config/friendships.php
+++ src/config/friendships.php
@@ -9,8 +9,8 @@ return [
 
     'groups' => [
         'acquaintances' => 0,
-        //'close_friends' => 1,
-        //'family' => 2
+        'close_friends' => 1,
+        'family' => 2
     ]
 
 ];
