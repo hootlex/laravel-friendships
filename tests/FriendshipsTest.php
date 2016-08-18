ff9e8be02a1856ed19ebcae7b07bf55ef7081e25
alex <hootlex@icloud.com>
2016-08-18 13:37:54 +0300

    bring back DatabaseTransactions

--- tests/FriendshipsTest.php
+++ tests/FriendshipsTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class FriendshipsTest extends TestCase
 {
-    // use DatabaseTransactions;
+    use DatabaseTransactions;
     
     /** @test */
     public function user_can_send_a_friend_request()
