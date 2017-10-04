23821c7ba1cd44c42f8d999ff84b22b1b9a50aa4
Nate Ritter <nate@perfectspace.com>
2017-10-04 14:03:10 -0700

    Trying out sqlite in memory instead.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,4 +9,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Setup DB before each test.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app['config']->set('database.default','sqlite');
+        $this->app['config']->set('database.connections.sqlite.database', ':memory:');
+    }
 }
